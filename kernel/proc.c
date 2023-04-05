7ee4f320273f131b2f7fd915773268fcbe87f69f
skyh-skyh <931544972@qq.com>
2023-04-05 20:52:57 +0800

    Lab: system calls(25/40points)

--- kernel/proc.c
+++ kernel/proc.c
@@ -298,6 +298,7 @@ fork(void)
 
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
+  np->trace_mask = p->trace_mask;
 
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
